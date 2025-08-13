c4f95636a73374394f72bb4cea27835d0293cb41
Bence1102 <szente.bence04@gmail.com>
2025-08-13 14:25:32 +0200

    1 kép megjelenitése

--- ReceptElonezet.js
+++ ReceptElonezet.js
@@ -5,5 +5,27 @@ export default class ReceptElonezet {
     #kep;
     #kedvenc;
     constructor(obj) {
+        this.#obj = obj
+        this.#szuloElem = document.querySelector(".jatekter");
+        this.#nev = obj.nev;
+        this.#kep = obj.kep;
+        this.#kedvenc = obj.kedvenc ? "Kedvenc" : "Nem kedvenc";
+        this.megjelenit();
+        //this.kedvencgomb();
+    }
+
+    megjelenit() {
+        const receptLista = document.createElement('div');
+        receptLista.innerHTML = `
+                <div class="receptkartya">
+                        <img src="${this.#kep}" alt="${this.#nev}">
+                    <div class= "receptbox">
+                        <h5 class="receptnev">${this.#nev}</h5>
+                        <button class="kedvenc">${this.#kedvenc}</button>
+                    </div>
+                </div>
+        `;
+        this.#szuloElem.appendChild(receptLista);
+
     }
 }
--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import { receptLista } from "./receptek.js";
 import ReceptElonezet from "./ReceptElonezet.js";
 
 
-
+new ReceptElonezet(receptLista[0]);
