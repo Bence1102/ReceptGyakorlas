35f52bd457204da550424fbb354547105de846ea
Bence1102 <szente.bence04@gmail.com>
2025-08-14 13:51:52 +0200

    hátra előre gomb megcsinálva

--- main.js
+++ main.js
@@ -6,4 +6,21 @@ import { receptLista } from "./receptek.js";
 new Recept(".jatekter");
 
 
-new NagyRecept(receptLista[0]);
+let index = 0;
+let jelenrecept= new NagyRecept(receptLista[index]);
+
+const eloregomb =document.querySelector(".elore");
+const hatragomb = document.querySelector(".hatra");
+eloregomb.addEventListener("click", () =>{
+    index++;
+    if(index >= receptLista.length) index=0;
+    document.querySelector(".adatok").innerHTML="";
+    jelenrecept=new NagyRecept(receptLista[index]);
+});
+
+hatragomb.addEventListener("click", () =>{
+    index++;
+    if(index >= receptLista.length) index=0;
+    document.querySelector(".adatok").innerHTML="";
+    jelenrecept=new NagyRecept(receptLista[index]);
+});
